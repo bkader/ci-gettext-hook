c1b8a4c1165a0f2c5ef831b68a45de5fc778282c
Kader Bouyakoub <bkader@mail.com>
2017-03-08 11:10:26 +0100

    Dealing with .gitignore

---
+++ application/controllers/Process.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * This is an example controller on how to switch language
+ * @package 	CodeIgniter\CI-Gettext
+ * @category 	Controllers
+ * @author 	Kader Bouyakoub <bkader@mail.com>
+ * @link 	http://www.bkader.com/
+ */
+
+class Process extends CI_Controller
+{
+	/**
+	 * Change site language
+	 * @access 	public
+	 * @param 	string 	$code 	code of the language to use
+	 * @return 	void
+	 */
+	public function lang($code = 'en')
+	{
+		function_exists('redirect') OR $this->load->helper('url');
+		
+		if (function_exists('switch_language')) {
+			switch_language($code);
+		}
+
+		redirect($this->input->get('next', TRUE));
+	}
+}
+
+/* End of file Process.php */
+/* Location: ./application/controllers/Process.php */
